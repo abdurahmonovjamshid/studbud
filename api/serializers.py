de8b4ea50eb4aa181fcafb3743e057369148c808
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-08 12:46:43 +0500

    creating a serializers package

---
+++ api/serializers.py
@@ -0,0 +1,8 @@
+from base.models import Room
+from rest_framework import serializers
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
