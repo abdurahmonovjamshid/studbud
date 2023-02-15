cf9ee9f732a6445b771017068563ece9a30236e4
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-15 13:43:00 +0500

    login and logout views for the browsable API.

--- api/urls.py
+++ api/urls.py
@@ -15,5 +15,7 @@ urlpatterns = [
     path('room/<int:pk>/messages', views.RoomMessages.as_view()),
     path('', include(router2.urls)),
     path('', include(router.urls))
-
+]
+urlpatterns += [
+    path('auth', include('rest_framework.urls')),
 ]
